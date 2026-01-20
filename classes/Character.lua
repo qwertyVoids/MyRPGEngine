d3181ecd4562cfbcc8ded4b66235187fc668d66b
Void10100 <ponomarenkogleb916@gmail.com>
2026-01-20 16:41:20 +0200

    Пуш 20.01.2026 16:41

--- classes/Character.lua
+++ classes/Character.lua
@@ -15,13 +15,38 @@ function Character.new(name, class)
     self._data.FistDamage = 8
     self._data.Money = 0
     self._data.Quests = {}
-    self._data.Modifiers = {}
+    self._data.Modifiers = {
+        {
+            name = "BaseHeal",
+            OnHeal = function(mod, event, owner)
+                owner = owner._data
+                if owner.Health <= 0 then
+                    print("Игрок умер! Лечение тут не поможет.")
+                    event.isBlocked = true
+                elseif owner.Health >= owner.MaxHealth>
+                    print("У игрока максимальное здоровье.")
+                    event.isBlocked = true
+                end
+            end
+        }
+    }
 
     return self
 end
 
 function Character:GetStat(statName)
     return self._data[statName]
+end
+
+function Character:HasModifier(modName)
+    local hasMod = false
+    for _, mod in ipairs(self._data.Modifiers) do
+        if mod.name:lower() == modName:lower() then
+            hasMod = true
+            break
+        end
+    end
+    return hasMod
 end
 
 function Character:ShowStats()
@@ -35,27 +60,26 @@ end
 function Character:TriggerEvent(eventName, eventData)
     for _, mod in ipairs(self._data.Modifiers) do
         if mod[eventName] then
-            mod[eventName](mod, eventData, self)
+            mod[eventName](mod, eventData or {}, self)
         end
     end
 end
 
 function Character:TakeDamage(amount)
-    if self._data.Health > 0 then
-        self._data.Health = math.max(self._data.Health - amount, 0)
-    else
-        print("Игрок " .. self._data.Name .. " уже умер!")
+    self._data.Health = math.max(self._data.Health - amount, 0)
+    if self._data.Health <= 0 then
+        table.insert(self._data.Modifiers, {
+            name = "Death",
+            OnIncomingDamage = function(mod, event, owner)
+                print("Игрок уже умер!")
+                event.isBlocked = true
+            end
+        })
     end
 end
 
 function Character:Heal(amount)
-    if self._data.Health < self._data.MaxHealth then
-        self._data.Health = math.min(self._data.Health + amount, self._data.MaxHealth)
-    elseif self._data.Health <= 0 then
-        print("Здоровье не изменилось. Игрок мёртв.")
-    elseif self._data.Health >= self._data.MaxHealth then
-        print("Здоровье не изменилось. У игрока полное здоровье.")
-    end
+    self._data.Health = math.min(self._data.Health + amount, self._data.MaxHealth)
 end
 
 function Character:UseItem(item)
